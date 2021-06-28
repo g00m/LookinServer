2f81913f2e4b84b4557ee1e3f906aaa999bd59f6
Etienne Negro <etienne.wojahn@me.com>
2021-06-28 10:53:58 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "LookinServer",
     platforms: [
         .iOS(.v9),
+        .tvOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
